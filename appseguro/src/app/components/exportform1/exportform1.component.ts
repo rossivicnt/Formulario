98b2b4a4920ac0601277b0a2db3f47e2fc3c93fa
Rosibel Vicent <rosi.vicent@gmail.com>
2018-07-31 21:55:15 -0430

    arreglo pdf y problemas en el forms

--- appseguro/src/app/components/exportform1/exportform1.component.ts
+++ appseguro/src/app/components/exportform1/exportform1.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild} from '@angular/core'; 
 import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-exportform1',
@@ -10,16 +11,24 @@ import * as jsPDF from 'jspdf';
 
 export class Exportform1Component {
 
-  @ViewChild('content') content: ElementRef;
-
   constructor(private elementRef: ElementRef) { 
   }
 
   public onClick(){
-    let doc = new jsPDF();
-    doc.addHTML(document.body,0,0,{ allowTaint: true }, () => {
-      doc.save("test.pdf");
-    } );
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208; 
+      var pageHeight = 295;  
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('MYPdf.pdf'); // Generated PDF 
+    });
   }
 
 }
